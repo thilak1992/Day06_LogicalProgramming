f9e5dad633281b7842d54896fb3173f6204c3af6
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-07-11 04:40:19 +0530

    Logical Programming/JUnitTesting

---
+++ src/com/Bridgelabz/Day06LogicalProblems_JUnitTesting/ToBinary.java
@@ -0,0 +1,30 @@
+package com.Bridgelabz.Day06LogicalProblems_JUnitTesting;
+
+import java.util.Scanner;
+
+public class ToBinary {
+
+    public static void main(String[] args) {
+        int number, i = 0;
+                                                            // array to store binary number
+        int binary[] = new int[1000];
+                                                            //Taking input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the decimal number: ");
+        number = sc.nextInt();
+
+                                                            //counter for binary array
+        while (number != 0) {
+                                                            // storing remainder in binary array
+            binary[i] = number % 2;
+            number = number / 2;
+            i++;
+        }
+
+        System.out.println("Binary value is: ");
+                                                            // printing binary array in reverse order
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(binary[j]);
+        }
+    }
+}
---
+++ src/com/Bridgelabz/Day06LogicalProblems_JUnitTesting/VendingMachine.java
@@ -0,0 +1,45 @@
+package com.Bridgelabz.Day06LogicalProblems_JUnitTesting;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+                                                                            // Static Variables i for indexing the array and total for calculating the total Notes
+    static int i = 0;
+    static int total = 0;
+
+                                                                            //Initialization of New Array
+    static int notes[] = { 1000,500,100,50,10,5,2,1};
+    static int money;
+
+                                                                            // Function for Calculating the notes
+    public static int calculate(int money,int[]notes ) {
+                                                                            //calling calculate Function
+        int remainder;
+
+        if (money == 0) {
+            return -1;
+        } else {
+                                                                            // for(int i = 0; i < notes.length; i++) {
+            if (money >= notes[i]) {
+                                                                            // logic for Calculating The notes
+                int calNotes = money / notes[i];
+                remainder = money % notes[i];
+                money = remainder;
+                total += calNotes;
+                System.out.println(notes[i] + " Notes ---> " + calNotes);
+            }
+            i++;
+            return calculate(money, notes);
+        }
+    }
+    public static void main(String[] args) {
+                                                                            //Taking amount from the user
+        System.out.println("Enter the amount: ");
+        Scanner sc = new Scanner(System.in);
+        money = sc.nextInt();
+
+                                                                            //Creating object for Vending Machine
+        VendingMachine.calculate(money,notes);
+        System.out.println("Total Number of Notes are :"+total);
+    }
+}
